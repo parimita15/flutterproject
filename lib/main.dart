73844d1689a3498f93cba5f3d9dc721a61dab061
parimita.nayak <parimita.nayak@gmail.com>
2022-11-12 17:16:49 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
